187881c823eb928177109fdabb4103268f0d9b94
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2025-11-21 11:10:08 +0000

    CI now runs shellcheck on all shell scripts

--- man2html.sh
+++ man2html.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
 
 /usr/bin/man ./hear.1 | ./cat2html > hear.1.html
--- upload_manpage.sh
+++ upload_manpage.sh
@@ -2,4 +2,4 @@
 
 REMOTE_USER=$1
 
-scp hear.1.html $REMOTE_USER@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/hear.1.html
+scp hear.1.html "$REMOTE_USER"@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/hear.1.html
